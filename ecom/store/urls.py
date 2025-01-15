01973ec54e04dd6cdcf3036a2b3067e3cc823f96
fahadrkm <fahadrkm@gmail.com>
2025-01-15 12:21:55 +0530

    added login and logout option.created login page and message for displaying login succesfully - added js timelimit  for messages

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -2,5 +2,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.home, name='home'),
-    path('about/',views.about, name='about')
+    path('about/',views.about, name='about'),
+    path('login/',views.login_user, name='login'),
+    path('logout/',views.logout_user, name='logout'),
 ]
