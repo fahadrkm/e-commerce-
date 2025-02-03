ed148e9f4bcac9b0dd52fafa696aae9796e5cc6b
fahadrkm <fahadrkm@gmail.com>
2025-02-03 12:18:34 +0530

    update shopping cart summary page

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -58,6 +58,15 @@ class Cart():
 
         thing = self.cart
         return thing
+    
+    def delete(self,product):
+        product_id = str(product)
+        #del from dic
+        if product_id in self.cart:
+            del self.cart[product_id]
+
+        self.session.modified = True
+
 
 
 
--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -36,10 +36,17 @@ def cart_add(request):
 
 
 def cart_delete(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        #get stuff
+        product_id = int(request.POST.get('product_id'))
+        #call del fun
+        cart.delete(product=product_id)
+        response =JsonResponse ({'product':product_id})
+        return response
 
 def cart_update(request):
-    cart =Cart(request)
+    cart = Cart(request)
     if request.POST.get('action') == 'post':
         #get stuff
         product_id = int(request.POST.get('product_id'))
