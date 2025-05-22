cc6b7df3cb371427785f733987a3292658c25fc7
fahadrkm <fahadrkm@gmail.com>
2025-05-22 14:36:35 +0530

    shipping model problem solved

--- ecom/payment/models.py
+++ ecom/payment/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
+from django.db.models.signals import post_save
+
 
 class ShippingAddress(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
@@ -20,6 +22,13 @@ class ShippingAddress(models.Model):
     def __str__(self):
         return f'shipping address - {str(self.id)}'
     
+#Create a  user shipping address by deafault
+def create_shipping(sender,instance,created,**kwargs):
+    if created:
+        user_shipping =ShippingAddress(user=instance)
+        user_shipping.save()
+post_save.connect(create_shipping, sender=User)
+
 
 #create order model
 class Order(models.Model):
--- ecom/store/models.py
+++ ecom/store/models.py
@@ -19,7 +19,7 @@ def create_profile(sender,instance,created,**kwargs):
     if created:
         user_profile =Profile(user=instance)
         user_profile.save()
-post_save.connect(create_profile,sender=User)
+post_save.connect(create_profile, sender=User)
         
 
 #cat of prdouct
