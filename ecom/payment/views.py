6d7e23ec58f584cf227a39468abe4c0eb96e0ac4
fahadrkm <fahadrkm@gmail.com>
2025-05-22 19:00:17 +0530

    Billing info

--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from cart.cart import Cart
 from payment.forms import ShippingForm
 from payment.models import ShippingAddress
+from django.contrib import messages
+from django.shortcuts import render, redirect
 # Create your views here.
 def payment_success(request):
     return render(request,"payment/payment_success.html",{})
@@ -25,4 +27,18 @@ def checkout(request):
         shipping_form = ShippingForm(request.POST or None)
         return render(request,"payment/checkout.html",{"cart_products":cart_products, 'quantities':quantities,"totals":totals,"shipping_form":shipping_form})
 
+def billing_info(request):
+    if request.POST:
+        #get cart
+        cart=Cart(request)
+        cart_products = cart.get_prods 
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+        shipping_form = request.POST
+        return render(request,"payment/billing_info.html",{"cart_products":cart_products, 'quantities':quantities,"totals":totals,"shipping_form":shipping_form})
+    else:
+        messages.success(request,"Access Denied")
+        return redirect('home')
+
+    
     
