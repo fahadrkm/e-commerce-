3e4a8e1a48579c483d116d761e542fc8de883336
fahadrkm <fahadrkm@gmail.com>
2025-05-23 19:22:16 +0530

    generate orders

--- ecom/payment/views.py
+++ ecom/payment/views.py
@@ -9,10 +9,44 @@ from store.models import Product, Profile
 
 def process_order(request):
     if request.POST:
-        pass
+        cart=Cart(request)
+        cart_products = cart.get_prods 
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        #get billing info from the last page
+        payment_form=Payment_Form(request.POST or None)
+        #get shipping session data
+        my_shipping = request.session.get('my_shipping')
+
+        #gather order info
+        full_name=my_shipping['shipping_fullname']
+        email=my_shipping['shipping_email']
+        #create shipping address from session
+        shipping_address=f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_pincode']}\n{my_shipping['shipping_country']}\n"
+        amount_paid=totals
+        #create an order
+        
+        if request.user.is_authenticated:
+            #logged in
+            user =request.user
+            #create order
+            create_order = Order(user=user,full_name=full_name,email=email,shipping_address=shipping_address,amount_paid=amount_paid)
+            create_order.save()
+            messages.error(request, "order placed")
+            return redirect('home')
+        else:
+            #not logged in
+            create_order = Order(user=user,full_name=full_name,email=email,shipping_address=shipping_address,amount_paid=amount_paid)
+            create_order.save()
+            messages.error(request, "order placed")
+            return redirect('home')
+        
 
     else:
-        pass
+        messages.error(request, "Access Denied")
+        return redirect('home')
+        
 
 def payment_success(request):
     return render(request,"payment/payment_success.html",{})
@@ -42,6 +76,11 @@ def billing_info(request):
         cart_products = cart.get_prods()
         quantities = cart.get_quants()
         totals = cart.cart_total()
+        
+        #create a session with shipping info
+        my_shipping =request.POST
+        request.session['my_shipping']=my_shipping
+
         #check user logged in
         if request.user.is_authenticated:
             #get the billing form
@@ -64,7 +103,22 @@ def billing_info(request):
             'billing_form':billing_form,
         })
              
+        #deleted form unwanted
+    else:
+        messages.error(request, "Access Denied")
+        return redirect('home')
 
+
+
+
+
+
+
+
+
+
+
+    '''  
         shipping_form = request.POST  # just pass raw data for now
 
         return render(request, "payment/billing_info.html", {
@@ -72,9 +126,5 @@ def billing_info(request):
             "quantities": quantities,
             "totals": totals,
             "shipping_form": shipping_form,
-        })
-    else:
-        messages.error(request, "Access Denied")
-        return redirect('home')
-    
+        })'''
     
