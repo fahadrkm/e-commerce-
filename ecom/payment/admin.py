ed7ff930813982fa95fccfc9f7b94224ac79baa0
fahadrkm <fahadrkm@gmail.com>
2025-05-24 12:18:30 +0530

    order items inline

--- ecom/payment/admin.py
+++ ecom/payment/admin.py
@@ -10,10 +10,13 @@ admin.site.register(OrderItem)
 #create an order item inline
 class OrderItemInine(admin.StackedInline):
     model=OrderItem
+    extra=0
 
 #extend oyr order model
 class OrderAdmin(admin.ModelAdmin):
     model=Order
+    readonly_fields=["date_ordered"]
+    fields=["user","full_name","email","shipping_address","amount_paid","date_ordered"]
     inlines=[OrderItemInine]
 #unregister order model
 admin.site.unregister(Order)
