c74ee51bca7f00ab6fc0df0f29cf3ade9c6fab99
fahadrkm <fahadrkm@gmail.com>
2025-05-28 20:18:39 +0530

    changes in billing info

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -69,14 +69,14 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': ,
-        'USER': ,
-        'PASSWORD': ,
-        'HOST': ,
-        'PORT': ,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        #''' 'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': ,
+        #'USER': ,
+        #'PASSWORD': ,
+        #'HOST': ,
+        #'PORT': , '''
 
         }
 }
